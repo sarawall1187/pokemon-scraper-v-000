031992e8882e480e69cbf046d6ffc9a73a798aad
Sara Walker <sarawall1187@gmail.com>
2019-02-12 01:30:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ attr_accessor :id, :name, :type, :db, :hp
  end
 
  def alter_hp(hp, db)
- db.execute("UPDATE pokemon SET hp = hp WHERE id = self.id;")
+   db.execute("UPDATE pokemon SET hp = hp WHERE id = self.id;")
  end
 
 end
